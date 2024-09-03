46ce4b3aa9ac132ba7e6934ca7259be23c542202
yu64 <yuma1212y@gmail.com>
2024-09-03 09:57:49 +0900

    Windowsアプリとコンソールアプリを両立できないため、Windowsアプリとして作成し、コマンド引数が異なる場合は、AttachConsoleするように変更

--- src/presentation/console/ConsoleView.cs
+++ src/presentation/console/ConsoleView.cs
@@ -1,28 +1,88 @@
 
+using System;
 using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Help;
 using System.CommandLine.Invocation;
 using System.CommandLine.NamingConventionBinder;
+using System.CommandLine.Parsing;
+using System.Diagnostics;
 using System.IO;
+using System.Management;
+using System.Runtime.InteropServices;
 
 namespace NotifiableTools;
 
 public class ConsoleView
 {   
     private ConsoleController controller;
+
     private readonly RootCommand root;
+    private readonly Parser parser;
 
 
     public ConsoleView(
         ConsoleController controller
     )
     {
+
         this.controller = controller;
+
         this.root = this.DefineCommand();
+        this.parser = new CommandLineBuilder(this.root)
+            .UseDefaults()
+            .Build();
     }
 
     public async Task<int> Start(string[] args)
     {
-        return await this.root.InvokeAsync(args);
+        //事前確認
+        var result = this.parser.Parse(args);
+        var hasError = (0 < result.Errors.Count);
+
+        if(!hasError)
+        {
+            //実際に実行
+            return await this.parser.InvokeAsync(args);
+        }
+
+        this.OpenConsole();
+
+        //既定のエラーメッセージを表示
+        foreach (var error in result.Errors)
+        {
+            Console.WriteLine(error.Message);
+        }
+
+        //ヘルプ
+        var helpBuilder = new HelpBuilder(LocalizationResources.Instance);
+        helpBuilder.Write(root, Console.Out);
+        return 1;
+    }
+
+
+//=====================================================================================================
+
+    [DllImport("Kernel32.dll")]
+    protected static extern Boolean AttachConsole(Int32 processId);
+    
+    [DllImport("Kernel32.dll")]
+    protected static extern Boolean AllocConsole();
+    
+    [DllImport("Kernel32.dll")]
+    protected static extern Boolean FreeConsole();
+
+    public void OpenConsole()
+    {
+        if(AttachConsole(-1))
+        {
+            return;
+        }
+
+        if(AllocConsole())
+        {
+            return;
+        }
     }
 
 
